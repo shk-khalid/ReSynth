388759dac24a69c0a5021b63388b76528924d52a
Khalid Shaikh <ks7850837@gmail.com>
2026-03-04 20:44:07 +0530

    feat: Integrate Tavily and DuckDuckGo search with fallback, replacing the previous Serper implementation.

--- app/services/search.py
+++ app/services/search.py
@@ -1,12 +1,58 @@
 import os
-import requests
+from tavily import TavilyClient
+from ddgs import DDGS
 from dotenv import load_dotenv
 
 load_dotenv()
 
 SERPER_API_KEY = os.getenv("SERPER_API_KEY")
+TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
 
-def search(query: str, num_results: int = 10):
+tavily = TavilyClient(api_key=TAVILY_API_KEY)
+
+def tavily_search(query: str, num_results: int = 10):
+    try:
+        response = tavily.search(
+            query=query,
+            search_depth="advanced",
+            max_results=num_results,
+        )
+
+        results = []
+
+        for r in response["results"]:
+            results.append({
+                "title": r["title"],
+                "link": r["url"],
+                "snippet": r["content"]
+            })
+
+        return results
+    except Exception as e:
+        print(f"Error in tavily_search: {e}")
+        return []
+    
+
+def duckduckgo_search(query: str, num_results: int= 5):
+
+    results = []
+
+    try:
+        with DDGS() as ddgs:
+            for r in ddgs.text(query, max_results=num_results):
+                results.append({
+                    "title": r["title"],
+                    "link": r["href"],
+                "snippet": r["body"]
+            })
+
+    except Exception as e:
+        print(f"Error in duckduckgo_search: {e}")
+        return []
+
+    return results
+
+def serper_search(query: str, num_results: int = 5):
     
     url = "https://google.serper.dev/search"
 
@@ -31,3 +77,18 @@ def search(query: str, num_results: int = 10):
         }
         for r in results.get("organic", [])
     ]
+
+def search(query: str, num_results: int = 5):
+    """
+    Primary search: Tavily
+    Fallback search: DuckDuckGo
+    """
+    # results = duckduckgo_search(query, num_results)
+
+    results = tavily_search(query, num_results)
+    if not results: 
+        print("Tavily failed. Falling back to DuckDuckGo.")
+        results = duckduckgo_search(query, num_results)
+
+    return results
+   
